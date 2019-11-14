35e48d7b80e7da1e47d7391d43d271e13661afa6
Approxomiron <nzubtsov194@gmail.com>
2019-11-14 23:07:21 +0300

    Add imports and getter to GornerTableCell

--- src/lab3/GornerTableCell.java
+++ src/lab3/GornerTableCell.java
@@ -1,5 +1,21 @@
 package lab3;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+public class GornerTableCell  implements TableCellRenderer {
 
-public class GornerTableCell {
+	@Override
+	public Component getTableCellRendererComponent(JTable arg0, Object arg1, boolean arg2, boolean arg3, int arg4,
+			int arg5) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
